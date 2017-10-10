37cabc3d3c35169da2796e9d2b3e06696fbfb2b8
GZzzhsmart <1729340612@qq.com>
2017-10-10 16:04:37 +0800

    删除

--- SSMShiro/src/main/java/com/th/mapper/SlaveMapper.java
+++
@@ -1,9 +0,0 @@
-package com.th.mapper;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SlaveMapper {
-    Integer count();
-
-}
--- SSMShiro/src/test/java/com/th/aop/Elephant.java
+++
@@ -1,7 +0,0 @@
-package com.th.aop;
-
-public class Elephant {
-    public void stealBanana(String name){
-        System.out.println("【大象】"+name+"正在偷香蕉...");
-    }
-}
