cf59480e34d6625badc85442fc535e678e1f2770
GZzzhsmart <1729340612@qq.com>
2017-10-12 19:19:31 +0800

    简单的activiti流程

--- JavaActiviti/src/main/java/com/th/activiti/Activiti.java
+++ JavaActiviti/src/main/java/com/th/activiti/Activiti.java
@@ -1,8 +1,13 @@
 package com.th.activiti;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngineConfiguration;
+import junit.framework.TestCase;
+import org.activiti.engine.*;
 import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
+import java.util.List;
+
 /**
  * ProcessEngine 流程引擎      所有的Activiti功能最终都要借用ProcessEngine来完成
  * ProcessEngineConfiguration   流程引擎配置类，主要是配置数据库
@@ -15,26 +20,92 @@ import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
  ACT_GE_*: 'GE'表示General。通用数据， 用于不同场景下。
  *
  */
-public class Activiti {
-    public static void main(String[] args) {
+public class Activiti extends TestCase{
+
+    private ProcessEngine processEngine;
+
+    @Override
+    protected void setUp() throws Exception {
+        /**
+         * ProcessEngineConfiguration为Activiti流程引擎的配置器类
+         */
         ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
                 .setJdbcUrl("jdbc:mysql://localhost:3306/activiti")
                 .setJdbcUsername("root")
                 .setJdbcPassword("123456")
                 .setJdbcDriver("com.mysql.jdbc.Driver")
+                // 设置是否自动更新数据库里的数据表
                 /**
-                 *    public static final String DB_SCHEMA_UPDATE_FALSE = "false";操作activiti25张表的时候，如果表不存在，就抛出异常，不能自动创建25张表
-
-                 public static final String DB_SCHEMA_UPDATE_CREATE_DROP = "create-drop";每次操作，都会先删除表，再创建表
-
-                 public static final String DB_SCHEMA_UPDATE_TRUE = "true";如果表不存在，就创建表，如果表存在，就直接操作
+                 * ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE   如果没有数据表，则创建数据表，如果有，则直接使用
+                 * ProcessEngineConfiguration.DB_SCHEMA_UPDATE_FALSE  如果没有数据表，则抛出异常，如果有，则直接使用
+                 * ProcessEngineConfiguration.DB_SCHEMA_UPDATE_CREATE_DROP  如果没有数据表，则创建数据表，如果有，则先删除再创建表
                  */
-                // 此时没有activiti的数据表，而是通过创建流程引擎的过程中创建数据表。设置创建数据表的策略
                 .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
-        // 创建流程引擎，在此过程中，ProcessEngine会自动在我们前面定义好的数据库中创建 activiti所需要的28张表
-        ProcessEngine processEngine = cfg.buildProcessEngine();
+        processEngine = cfg.buildProcessEngine(); // 通过配置器构建流程引擎，activiti自动到配置的数据库中创建28张表
         String pName = processEngine.getName();
         String ver = ProcessEngine.VERSION;
         System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
     }
+
+    @Test
+    public void testDeploy() {
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.createDeployment().addClasspathResource("diagrams/goods_apply.bpmn20.xml").deploy(); //  先读取流程定义文件，再完成部署
+    }
+
+    @Test
+    public void testStartProcess() {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("goods_apply"); // 启动流程
+    }
+
+    @Test
+    public void testListTask() {
+        // 获取当前需要执行的任务，用于查询出指定用户的所有待办任务
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList =  taskService.createTaskQuery().list();
+        for (Task task : taskList) {
+            task.setAssignee("emp1"); // 把任务指派给指定的用户
+            taskService.complete(task.getId()); // 完成任务
+            System.out.println(task.getName());
+        }
+    }
+
+    @Test
+    public void testLeaveProcessDeploy() {
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.createDeployment().addClasspathResource("diagrams/leave_process.bpmn20.xml").deploy();
+    }
+
+    @Test
+    public void testStartLeaveProcess() {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        runtimeService.startProcessInstanceByKey("leave_process");
+    }
+
+    /**
+     * assignee 指派的人
+     * candidate 候选人
+     */
+    @Test
+    public void testSubmit() {
+        TaskService taskService = processEngine.getTaskService();
+        // taskAssinee是指只去查找指定用户的任务。
+        // 根据登录的用户名去查找此用户的待办任务
+        List<Task> taskList = taskService.createTaskQuery().taskAssignee("emp").list();
+        for (Task task : taskList) {
+            taskService.complete(task.getId()); // 用户去执行此用户下的所有任务
+        }
+    }
+
+    @Test
+    public void testCheck() {
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList = taskService.createTaskQuery().taskAssignee("boss").list();
+        for (Task task : taskList) {
+            taskService.complete(task.getId());
+        }
+    }
+
+
 }
