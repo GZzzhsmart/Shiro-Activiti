377b66e1149d7243e4f75fe9049ce39e20eca3b5
GZzzhsmart <1729340612@qq.com>
2017-10-13 11:25:55 +0800

    多用户请假流程

---
+++ JavaActiviti/src/main/java/com/th/bean/User.java
@@ -0,0 +1,33 @@
+package com.th.bean;
+
+
+public class User {
+
+    private String name;
+    private String role;
+
+    public User() {
+
+    }
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
